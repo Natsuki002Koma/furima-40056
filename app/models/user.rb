90af334cb319c3786a44854e20378b63be36f7b9
Natsuki002Koma <furusawa.n.002@gmail.com>
2023-11-16 13:20:53 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,10 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :date_of_birth, presence: true
 
-  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]{6,}\z/i, message: '半角英数字（混合）を使用してください' } do
+  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,}\z/i, message: '半角英数字（混合）を使用してください' } do
     validates :password
   end
-  
+
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）を使用してください' } do
     validates :last_name
     validates :first_name
