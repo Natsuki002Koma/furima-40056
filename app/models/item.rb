484d065b6eb391a86304cd04b7b9c1eb326f93b4
Natsuki002Koma <furusawa.n.002@gmail.com>
2023-11-17 11:40:09 +0900

    実装条件の追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,7 +9,9 @@ class Item < ApplicationRecord
   belongs_to :prefecture
   belongs_to :shipping_day
 
-  validates :item_name, :description, :price, :image, presence: true
+  validates :item_name, :description, :image, presence: true
   validates :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 } 
-
+  
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  
 end
