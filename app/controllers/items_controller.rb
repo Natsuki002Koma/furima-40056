28e1eeac012159468b65ce29edb855d213aade01
Natsuki002Koma <furusawa.n.002@gmail.com>
2023-11-20 19:03:25 +0900

    商品削除機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
-  before_action :set_item, only: [:edit, :update, :move_to_index]
+  before_action :set_item, only: [:edit, :update, :destroy, :move_to_index]
   before_action :move_to_index, only: :edit
 
   def index
@@ -34,6 +34,11 @@ class ItemsController < ApplicationController
     else
       render :edit, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @item.destroy
+    redirect_to root_path
   end
 
   private
