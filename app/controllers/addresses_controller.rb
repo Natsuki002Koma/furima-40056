86636fa9340f32ab58c6e3a535b95351ef72f08f
Natsuki002Koma <furusawa.n.002@gmail.com>
2023-11-23 15:21:01 +0900

    orders、addressesコントローラーの設定、ビューの設定

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,17 @@
 class AddressesController < ApplicationController
+  def create
+    @order = order.find(params[:order_id])
+    @address = Order.address.new(address_params) 
+    @address.save
+    if @address.save
+      redirect_to root_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def address_params
+    params.require(:oder).permit(:post_code, :prefecture_id, :city, :street, :building, :tel).merge(order_id: @order.id)
+  end
 end
--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @order = Order.new
+    @address = Address.new
+  end
+
+  def create
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to root_path
+    else
+      render  :index
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).merge(user_id: current_user.id, item_id: @item.id)
+  end
 end
